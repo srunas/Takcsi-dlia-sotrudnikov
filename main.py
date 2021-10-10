1d64bd518c31ea8eb5348b42030107f1034b5ecb
Dima Strunevsiy <89925844+srunas@users.noreply.github.com>
2021-10-10 23:31:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 print("Программа ТаксиСотрудники")
-while(1):
-    staff = int(input("Введите количетсво сотрудников(от 1 до 1000: "))
-    if 1 <= staff <= 1000:
-        staffX = []
-        n = 0
-        for i in range(1, staff+1):
-            staffX.append(i)
-        tariffs = sorted([int(s) for s in input("Введите через пробел тарифы такси: ").split()])
-        distances = sorted([int(s) for s in input("Введиде дистанцию в км (через пробел): ").split()], reverse=True)
-        # ПО СУТИ ВЕСЬ СМЫСЛ ПРОГРАММЫ ЗАЛОДЕН В ВЕРХНИХ 3Х СТРОЧКАХ,
-        # работают они по принципу сортировки через i и в целом понятно и хорошо работают
-        print(sum(t*d for t, d in zip(tariffs, distances)), 'рублей')
-        for i in range(len(staffX)):
-            print(staffX[i], "Сотрудник/Тариф такси", tariffs[i], 'рублей/Машина №', n + i+1)
-    else: print("Вы ввели не верное количество сотрудников, попробуйте еще раз :D")
-    # Верхняя строчка чистейшей воды выпендреж так что не обращайте внимание)
+while True:
+    try:
+        staff = int(input("Введите количетсво сотрудников(от 1 до 1000): "))
+        break
+    except(ValueError, TypeError):
+        print(print('Вы ввели неверное значение'))
+taxi = {}
+distances = {}
+j = 0
+g = 0
+for i in range(staff):
+    g = g + 1
+    distances[g] = input("Введиде дистанцию в км : ")
+sorted_distances = sorted(distances.items(), key=lambda x: x[1], reverse=True)
+for i in range(staff):
+    j = j + 1
+    taxi[j] = input("Введите тариф такси: ")
+sorted_taxi = sorted(taxi.items(), key=lambda x: x[1])
+print("Номер сотрудника и его км:")
+print(sorted_distances)
+print("Номер машины куда должен сесть сотрудник и ее тариф:")
+print(sorted_taxi)

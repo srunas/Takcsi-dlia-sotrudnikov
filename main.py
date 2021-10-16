0de5e2182a6a9ccdadc7adcce8b3e2380eac3c12
Dima Strunevsiy <89925844+srunas@users.noreply.github.com>
2021-10-17 01:27:41 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ while True:
     except(ValueError, TypeError):
         print(print('Вы ввели неверное значение'))
 taxi = {}
-distances = {}
+dict_list = []
+distances = {}# Тут программа создает пустые словари для дальнейшего добавления в них значений.
 j = 0
 g = 0
 for i in range(staff):
     g = g + 1
-    distances[g] = input("Введиде дистанцию в км : ")
-sorted_distances = sorted(distances.items(), key=lambda x: x[1], reverse=True)
+    distances[g] = input("Введиде дистанцию в км : ")# Здесь я сделал чтобы программа добовляет значение по ключу в пустой словарь.
+sorted_distances = sorted(distances.items(), key=lambda x: x[1])# Ну а тут происходит сортировка по 2му значению в словаре.
 for i in range(staff):
     j = j + 1
-    taxi[j] = input("Введите тариф такси: ")
-sorted_taxi = sorted(taxi.items(), key=lambda x: x[1])
-print("Номер сотрудника и его км:")
-print(sorted_distances)
-print("Номер машины куда должен сесть сотрудник и ее тариф:")
-print(sorted_taxi)
+    taxi[j] = input("Введите тариф такси: ")# Все тоже самое только для трифов такси.
+sorted_taxi = sorted(taxi.items(), key=lambda x: x[1], reverse=True)
+for i in range(staff):
+    my_dict = {item[0]: item[1] for item in zip(sorted_distances, sorted_taxi)}# Объединяю все в один список.
+    dict_list.append(my_dict)# Добовляю все в один список.
+print("Номер сотрудника/км, Номер машины/тариф")
+print('\n'.join(str(el) for el in my_dict.items()),)
+# выводится номер сотрудника и его км, а далее номер такси и его тариф
